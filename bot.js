d008edaef047f7a5eafbd7244eac57786e971c6a
GameFozer05 <47607641+GameFozer05@users.noreply.github.com>
2019-02-27 22:04:55 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("550422507702714379")
+setInterval(function() {
+channel.send(``);
+}, 30)
+})
+
+client.login(NTUwNDIyMjg3NzExNzMxNzEz.D1iMOA.Bl732Q9PiAFno74a_N5IVyp_BGM);
